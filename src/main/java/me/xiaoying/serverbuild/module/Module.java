9eab57644f79b788108eb3c621cefb1f65d3c35a
Hanhan <764932129@qq.com>
2024-07-22 01:02:01 +0800

    feat: make module support Command

--- src/main/java/me/xiaoying/serverbuild/module/Module.java
+++ src/main/java/me/xiaoying/serverbuild/module/Module.java
@@ -1,10 +1,18 @@
 package me.xiaoying.serverbuild.module;
 
+import me.xiaoying.serverbuild.command.Command;
+import me.xiaoying.serverbuild.command.RegisteredCommand;
+import me.xiaoying.serverbuild.command.SCommand;
 import me.xiaoying.serverbuild.core.SBPlugin;
 import me.xiaoying.serverbuild.file.File;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,8 +20,9 @@ import java.util.List;
 public abstract class Module {
     private boolean opened = false;
 
-    private final List<Listener> listeners = new ArrayList<>();
     private final List<File> files = new ArrayList<>();
+    private final List<SCommand> commands = new ArrayList<>();
+    private final List<Listener> listeners = new ArrayList<>();
 
     /**
      * Get name of Module
@@ -79,11 +88,46 @@ public abstract class Module {
         file.load();
     }
 
+    /**
+     * Get registered commands of Module
+     *
+     * @return ArrayList
+     */
+    public List<SCommand> getCommands() {
+        return this.commands;
+    }
+
+    /**
+     * Register Command
+     *
+     * @param command SCommand
+     */
+    public void registerCommand(SCommand command) {
+        if (this.commands.contains(command))
+            return;
+
+        this.commands.add(command);
+    }
+
     public abstract void init();
 
     public void enable() {
         // register listeners
         this.listeners.forEach(listener ->  Bukkit.getPluginManager().registerEvents(listener, SBPlugin.getInstance()));
+        // register commands
+        this.commands.forEach(command -> {
+            Command annotation = command.getClass().getAnnotation(Command.class);
+
+            if (annotation == null) {
+                Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&eFined some command(" + command.getClass().getName() + ") don't use Command annotation, please check your code!"));
+                return;
+            }
+
+            for (String value : annotation.values()) {
+                SBPlugin.registerCommand(value, SBPlugin.getInstance());
+                SBPlugin.getInstance().getCommand(value).setExecutor(command.getTabExecutor());
+            }
+        });
 
         this.opened = true;
     }
